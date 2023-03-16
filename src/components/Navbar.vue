aa63b5dbb15950f8f120dab3c55e1cef615370fb
unknown <ojasinamdar101@gmail.com>
2023-03-16 12:27:01 +0530

    [feat]: 16/03

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-app-bar app>
+        <div class="d-flex align-center">
+            <v-img alt="Vuetify Logo" class="shrink mr-2" contain src="@/assets/logo.jpg" transition="scale-transition"
+                width="40" />
+
+            {{ $const.APP_NAME }}
+        </div>
+
+        <v-spacer></v-spacer>
+
+        <v-btn href="/login" text>
+            <span class="mr-2">Login</span>
+            <v-icon>mdi-account-circle</v-icon>
+        </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="center" :style="`background-color: ${randomColor()}`">
+        <h1>Login Page</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
